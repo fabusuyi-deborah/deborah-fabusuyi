024f7ea2d0c49bef2254cef3a4eedf5639d19538
fabusuyi-deborah <fabusuyideborah03@gmail.com>
2025-09-13 16:26:04 +0100

    fix : fixed an overflow

--- src/components/sections/Articles.tsx
+++ src/components/sections/Articles.tsx
@@ -14,7 +14,7 @@ type ArticlesSectionProps = {
 
 export default function ArticlesSection({ articles }: ArticlesSectionProps) {
   return (
-    <section className="py-10 md:py-16 mt-20  max-w-7xl" id="articles">
+    <section className="py-10 md:py-16 mt-20  max-w-7xl overflow-auto" id="articles">
       <h1 className="text-lg md:text-5xl font-bold py-10 text-center ">
         <span className="block">PUBLICATIONS & ARTICLES</span>
       </h1>
